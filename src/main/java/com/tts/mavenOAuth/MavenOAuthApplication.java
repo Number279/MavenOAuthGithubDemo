cbca6ce027d14a86d3c7be1d8dfffac791eb5c37
Number279 <sawyercarroll@gmail.com>
2021-02-09 17:03:59 -0600

    github login works with OAuth on Maven

---
+++ src/main/java/com/tts/mavenOAuth/MavenOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.tts.mavenOAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenOAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/mavenOAuth/MavenOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.mavenOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
